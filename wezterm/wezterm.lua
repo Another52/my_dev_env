f5631d2fc15c9c7cc46e8804d4dda97cddc7aa97
Another52 <syrincofficial446@gmail.com>
2025-10-21 01:08:45 +0530

    use opacity variable (useless commit)

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,8 @@
-local wezterm = require 'wezterm'
+-- variables
+local opacity = 0.7
+--
 
+local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- shortcut keys
@@ -14,7 +17,13 @@ config.keys = {
 -- window decor
 config.color_scheme = 'Atom'
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-config.window_background_opacity = 0.7
+config.window_background_opacity = opacity
+
+config.hide_tab_bar_if_only_one_tab = true
+config.window_frame = {
+    active_titlebar_bg = string.format("rgba(0 0 0 %f)", opacity),
+    inactive_titlebar_bg = string.format("rgba(0 0 0 %f)", opacity),
+}
 
 wezterm.on("toggle-opacity", function(window, pane)
     local overrides = window:get_config_overrides() or {}
