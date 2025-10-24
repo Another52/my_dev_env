6b1b9858319efc671598d6758923bf6172f1b469
Another52 <syrincofficial446@gmail.com>
2025-10-24 22:53:47 +0530

    zsh is default on linux

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -35,6 +35,11 @@ wezterm.on("toggle-opacity", function(window, pane)
     window:set_config_overrides(overrides)
 end)
 
+-- Linux: launch ZSH
+if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
+    config.default_prog = { 'zsh' }
+end
+
 -- Windows: launch MSYS2 Clang64 via cmd.exe
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
     config.default_cwd = "D:/Code"
